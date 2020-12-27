fe8c7ff4b8930d59dc860aed0cc178de6c7ab3f3
Sebastian Hoyer <shoyer@averroesconcept.onmicrosoft.com>
2020-12-27 10:06:32 +0100

    Beautifying

--- blazing-IT.Components/CssClassAttribute.cs
+++ blazing-IT.Components/CssClassAttribute.cs
@@ -7,30 +7,55 @@ using System.Threading.Tasks;
 
 namespace blazing_IT.Components
 {
+    /// <summary>
+    /// This attribute can be used in enums to man an enum into CSS classes. 
+    /// These enums are usefull to pass them as parameter into Razor Components.
+    /// </summary>
+    /// <example>
+    /// https://github.com/SebHoyer/blazing-IT.Components.FontAwesome/blob/master/blazing-IT.Components.FontAwesome/FaIconSize.cs
+    /// </example>
+    [AttributeUsage(AttributeTargets.Enum)]
     public class CssClassAttribute : Attribute
     {
         private string _cssClassName;
-        public CssClassAttribute(string cssClassName)
-        {
-            _cssClassName = cssClassName;
-        }
-
+        /// <summary>
+        /// The name of the CSS class, the enum-value gets mapped into.
+        /// </summary>
         public string CssClassName
         {
             get { return _cssClassName; }
         }
 
-        public static string GetCssClass(Enum value)
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="cssClassName">The name of the CSS class, the enum-value gets mapped into.</param>
+        public CssClassAttribute(string cssClassName)
         {
-            string output = null;
-            Type type = value.GetType();
-            FieldInfo fi = type.GetField(value.ToString());
-            CssClassAttribute[] attrs = fi.GetCustomAttributes(typeof(CssClassAttribute), false) as CssClassAttribute[];
-            if (attrs.Length > 0)
+            _cssClassName = cssClassName;
+        }
+
+        /// <summary>
+        /// Resolves the CSS class from a given enum-value.
+        /// The enum has to be decorated with the CssClassAttribute
+        /// </summary>
+        /// <param name="enumValue">The enum-value to resolve</param>
+        /// <returns></returns>
+        public static string GetCssClass(Enum enumValue)
+        {
+            Type enumValueType = enumValue.GetType();
+            Type cssClassAttributeType = typeof(CssClassAttribute);
+
+            FieldInfo enumValueFieldInfo = enumValueType.GetField(enumValue.ToString());
+            CssClassAttribute[] enumValueAttributes = enumValueFieldInfo.GetCustomAttributes(cssClassAttributeType) as CssClassAttribute[];
+
+            string cssClassName = string.Empty;
+            if (enumValueAttributes.Length > 0)
             {
-                output = attrs[0].CssClassName;
+                cssClassName = enumValueAttributes[0].CssClassName;
             }
-            return output;
+
+            return cssClassName;
         }
     }
 
